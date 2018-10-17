ada3c545bbb349843edd52dd031efa1c09e8690e
Carl Reed <carl.w.reed@vumc.org>
2018-10-17 14:49:00 -0500

    changes

--- js/userRoles.js
+++ js/userRoles.js
@@ -3,9 +3,17 @@ $(function() {
 	UserRoles.roles = JSON.parse($("#rolesData").html())
 	
 	// initialize interface with newly stored roles information
+	var falseCheckbox = "<input type=\"checkbox\">"
+	var trueCheckbox = "<input type=\"checkbox\" checked>"
 	for (var role in UserRoles.roles) {
-		let tableRow = ""
-		$("#rolesTable tbody").append(button)
+		let tableRow = `
+		<tr>
+			<td>${role}</td>
+			<td>${UserRoles.roles[role].active ? trueCheckbox : falseCheckbox}</td>
+			<td>${UserRoles.roles[role].external ? trueCheckbox : falseCheckbox}</td>
+		</tr>
+		`
+		$("#rolesDiv table tbody").append(tableRow)
 	}
 })
 
